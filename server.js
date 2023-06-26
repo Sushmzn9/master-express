a3f5a8136c12ca6b5637ca15bfce1ceff723cf03
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-06-26 23:24:07 +1000

    update

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import path from "path";
+const app = express();
+const PORT = process.env.PORT || 8000;
+const __dir = path.resolve();
+import bodyParser from "body-parser";
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/v1/userdata", (req, res) => {
+  res.json({
+    status: "success",
+    message: "server is running healthy",
+  });
+});
+app.get("/", (req, res) => {
+  res.send("This is about");
+});
+
+// app.post("/api/v1/login", (req, res) => {
+//   //   const userData = req.body.name;
+//   res.send("done");
+//   console.log(req.body);
+// });
+
+//CURD Operation GET POST PUT DElete
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log("error")
+    : console.log(
+        `your server is healthy and running at http://localhost:${PORT}`
+      );
+});
